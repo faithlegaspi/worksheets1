5b410b780ee5036bd7208c93e630a54b206da529
faithlegaspi <faith.legaspi@students.isat.edu.ph>
2023-09-21 02:12:11 +0000

    worksheet

---
+++ worksheet1.R
@@ -0,0 +1,33 @@
+age<-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,35, 31, 27, 22, 37, 
+       34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,51, 
+       35, 24, 33, 41.)
+
+sort (age)
+
+seq (age)
+
+#a. how many data points?
+#b. write the R code and its output
+min(age)
+max(age)
+sum(age)
+sorted_age <- sort(age)
+#filter(Age)
+
+reciproc_age <-1/age
+reciproc_age
+
+
+#2. Find the reciprocal of the values for age.
+reciproc_age <-1/age
+reciproc_age
+
+#3. Assign also new_age <- c(age, 0, age).
+new_age <- c(age,0,age)
+
+#4. Sort the values for age.
+sort(age)
+
+#5. Find the minimum and maximum value for age.
+min(age)
+max(age)
